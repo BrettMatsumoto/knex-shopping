91968e124dd723c0c42f506a598d7225377be051
Brett <brett_matsumoto@yahoo.com>
2019-05-02 08:42:59 -1000

    added use strict

--- seeds/02_products.js
+++ seeds/02_products.js
@@ -1,9 +1,11 @@
 
+'use strict';
+
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
+
   return knex('products').del()
     .then(function () {
-      // Inserts seed entries
+
       return knex('products').insert([
         {title: 'sometitle', description: 'stuff', inventory: 1, price: 3.20},
         {title: 'sometitle', description: 'stuff', inventory: 1, price: 3.20},
--- seeds/03_carts.js
+++ seeds/03_carts.js
@@ -1,9 +1,10 @@
+'use strict';
 
 exports.seed = function(knex, Promise) {
-  //Deletes ALL existing entries
+
   return knex('carts').del()
     .then(function () {
-      // Inserts seed entries
+
       return knex('carts').insert([
         {user_id: 1, product_id: 1},
         {user_id: 2, product_id: 2},
