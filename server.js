e6d9d28228256d3fa118652fa45f861c58a77104
Brett <brett_matsumoto@yahoo.com>
2019-05-02 08:54:00 -1000

    added requires for users/products/carts routes

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 'use strict';
 
+const knex = require('./database');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const urlParser = bodyParser.urlencoded({extended:false});
+const PORT = 3000;
+
+const users = require('./routes/users');
+const products = require('./routes/products');
+const carts = require('./routes/carts');
+
+knex.select().from('users').then((result) => {
+  console.log(result);
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`Express is listening on port ${PORT}.`)
+});
